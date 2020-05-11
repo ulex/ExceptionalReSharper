f3c7d68ede308ca2d6935aa3b0dee214e78284f4
Mahmoud Fakhoury <mahmoud.fakhoury@ulb.hhu.de>
2020-05-11 09:25:58 +0200

    Update SDK 2020.2 & Nuget Package

--- build/increment-build-version.ps1
+++ build/increment-build-version.ps1
@@ -1,4 +1,4 @@
-$workspace = ([System.IO.FileInfo] $PSCommandPath).Directory.Parent.FullName
+$workspace = ($PSCommandPath).Directory.Parent.FullName
 $solutionDirectory = Join-Path $workspace "src"
 $solutionPath = Join-Path $solutionDirectory "Exceptional.sln"
 $projectDirectory = Join-Path $solutionDirectory "Exceptional"
@@ -17,4 +17,4 @@ $newVersion = [version]::new($oldVersion.Major, $oldVersion.Minor, $newBuildVers
 $directoryBuildProperties.Project.PropertyGroup.Version = $newVersion.ToString()
 $directoryBuildProperties.Save($directoryBuildPropertiesPath)
 
-MSBuild $solutionPath /t:Exceptional:UpdateAssemblyInfo
+MSBuild.exe $solutionPath /t:Exceptional:UpdateAssemblyInfo
